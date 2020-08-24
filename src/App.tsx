482508b6d4b521341bfe82855a5a55f9b03a6760
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-08-24 12:18:35 +0200

    Removed dead code & files and changes post to fit Node-post

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios'
+
 function App() {
+
+  useEffect(()=> {
+    axios.post('http://localhost:8000/', {
+      firstName: 'Hanna',
+      lastName: 'Goldhammer',
+      email: 'hanna@gmail.com',
+      phone: 13371379012,
+      date: '21-12-22',
+      time: '21.00',
+      quantity: 133,
+      message: 'jättemkt allergier',
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
