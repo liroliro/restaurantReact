4b65b9923f74db9feeb9393c9f631fdb931eab06
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-08-26 11:40:00 +0200

    Start of Date-component

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+// let dateControl = document.querySelector('input[type="date"]');
+// dateControl.value = '2017-06-01';
 
 export default function Date() {
-	return <div>Date works</div>;
+  function todayDate(): void {
+    var date = Date().getDate();
+  }
+  return (
+    <>
+      <div>
+        <button value={Date}>Idag</button>
+        <button>Imorgon</button>
+        <button>Dagen efter imorgon haha</button>
+        <div>
+          <input type='date' id='date' name='date' />
+        </div>
+      </div>
+    </>
+  );
 }
