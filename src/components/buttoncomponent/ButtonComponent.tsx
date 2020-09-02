6921cff4e90a343bc629293709b5e29c15cf97de
Oscar Mattsson <oscar.mattsson@accodeing.com>
2020-09-02 15:21:42 +0200

    Removed buttoncomponent

--- src/components/buttoncomponent/ButtonComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface IButtonComponentProps {
-	value: string;
-}
-
-export default function ButtonComponent(props: IButtonComponentProps) {
-	return <button value={props.value}>{props.value}</button>;
-}
