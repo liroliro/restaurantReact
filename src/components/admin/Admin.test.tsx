c0f170eb6397894676e4ddb95d5833f11f31eded
Oscar Mattsson <oscar.mattsson@accodeing.com>
2020-09-08 09:34:47 +0200

    Some testing added

---
+++ src/components/admin/Admin.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import Admin from './Admin';
+
+test('Should contain h2s', () => {
+	const { container } = render(
+		<Admin
+			allBookings={[
+				{
+					_id: 'blabla',
+					date: '19/8',
+					time: 18,
+					guests: 18,
+					message: 'Hejhej',
+					customerId: 1234567890,
+				},
+			]}
+			allCustomers={[
+				{
+					id: '1',
+					firstName: 'Oscar',
+					lastName: 'Mattsson',
+					email: 'hello@gmail.com',
+					phone: '1234567890',
+				},
+			]}
+		/>
+	);
+
+	let h2element = container.querySelector('h2');
+	expect(h2element).toBeInTheDocument();
+});
+
+test('should contain string Datum', () => {
+	const { getByText } = render(
+		<Admin
+			allBookings={[
+				{
+					_id: 'blabla',
+					date: '19/8',
+					time: 18,
+					guests: 18,
+					message: 'Hejhej',
+					customerId: 1234567890,
+				},
+			]}
+			allCustomers={[
+				{
+					id: '1',
+					firstName: 'Oscar',
+					lastName: 'Mattsson',
+					email: 'hello@gmail.com',
+					phone: '1234567890',
+				},
+			]}
+		/>
+	);
+
+	let dateString = getByText('Datum');
+	expect(dateString).toBeInTheDocument;
+});
