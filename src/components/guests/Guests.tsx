0672387130c4f06321d5a889018117f200bdc772
Hanna <[email address]>
2020-09-09 14:42:28 +0200

    Accept changes

--- src/components/guests/Guests.tsx
+++ src/components/guests/Guests.tsx
@@ -26,69 +26,38 @@ export default function Guests(props: IGuestsProps) {
 	return (
 		<React.Fragment>
 			<div className='guestsConstainer'>
-<<<<<<< HEAD
 				<div className="firstSix">
 
 					<div className="buttonContainer">
 						<button value='1' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-				<div className='firstSix'>
-					<div className='buttonContainer'>
-						<button value='1' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							1
 						</button>
 					</div>
-<<<<<<< HEAD
 					<div className="buttonContainer">
 						<button value='2' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='buttonContainer'>
-						<button value='2' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							2
 						</button>
 					</div>
-<<<<<<< HEAD
 					<div className="buttonContainer">
 						<button value='3' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='buttonContainer'>
-						<button value='3' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							3
 						</button>
 					</div>
 
-<<<<<<< HEAD
 					<div className="buttonContainer">
 						<button value='4' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='buttonContainer'>
-						<button value='4' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							4
 						</button>
 					</div>
 
-<<<<<<< HEAD
 					<div className="buttonContainer">
 						<button value='5' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='buttonContainer'>
-						<button value='5' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							5
 						</button>
 					</div>
 
-<<<<<<< HEAD
 					<div className="buttonContainer">
 						<button value='6' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='buttonContainer'>
-						<button value='6' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 							6
 						</button>
 					</div>
@@ -101,70 +70,39 @@ export default function Guests(props: IGuestsProps) {
 					) : null}
 				</div>
 				{showLastSix ? (
-<<<<<<< HEAD
 					<div className="lastSix">
 						<div className="buttonContainer">
 							<button value='7' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-					<div className='lastSix'>
-						<div className='buttonContainer'>
-							<button value='7' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								7
 							</button>
 						</div>
 
-<<<<<<< HEAD
 						<div className="buttonContainer">
 							<button value='8' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-						<div className='buttonContainer'>
-							<button value='8' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								8
 							</button>
 						</div>
 
-<<<<<<< HEAD
 						<div className="buttonContainer">
 							<button value='9' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-						<div className='buttonContainer'>
-							<button value='9' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								9
 							</button>
 						</div>
 
-<<<<<<< HEAD
 						<div className="buttonContainer">
 							<button value='10' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-						<div className='buttonContainer'>
-							<button value='10' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								10
 							</button>
 						</div>
 
-<<<<<<< HEAD
 						<div className="buttonContainer">
 							<button value='11' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-						<div className='buttonContainer'>
-							<button value='11' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								11
 							</button>
 						</div>
 
-<<<<<<< HEAD
 						<div className="buttonContainer">
 							<button value='12' onClick={handleOptions} type='button' className='unclicked-guest'>
-=======
-						<div className='buttonContainer'>
-							<button value='12' onClick={handleOptions} type='button'>
->>>>>>> d2108e340a3ddb46dc635944922c8edca192d191
 								12
 							</button>
 						</div>
