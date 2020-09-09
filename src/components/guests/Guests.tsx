3390d18e9251afc2051273c222d3d92d1c30dc09
Hanna <[email address]>
2020-09-09 13:40:30 +0200

    lagg tillbaka style på knappar

--- src/components/guests/Guests.tsx
+++ src/components/guests/Guests.tsx
@@ -33,35 +33,35 @@ export default function Guests(props: IGuestsProps) {
 				<div className="firstSix">
 
 					<div className="buttonContainer">
-						<button value='1' onClick={handleOptions} type='button'>
+						<button value='1' onClick={handleOptions} type='button' className='unclicked-guest'>
 							1
 				</button>
 					</div>
 					<div className="buttonContainer">
-						<button value='2' onClick={handleOptions} type='button'>
+						<button value='2' onClick={handleOptions} type='button' className='unclicked-guest'>
 							2
 				</button>
 					</div>
 					<div className="buttonContainer">
-						<button value='3' onClick={handleOptions} type='button'>
+						<button value='3' onClick={handleOptions} type='button' className='unclicked-guest'>
 							3
 				</button>
 					</div>
 
 					<div className="buttonContainer">
-						<button value='4' onClick={handleOptions} type='button'>
+						<button value='4' onClick={handleOptions} type='button' className='unclicked-guest'>
 							4
 				</button>
 					</div>
 
 					<div className="buttonContainer">
-						<button value='5' onClick={handleOptions} type='button'>
+						<button value='5' onClick={handleOptions} type='button' className='unclicked-guest'>
 							5
 				</button>
 					</div>
 
 					<div className="buttonContainer">
-						<button value='6' onClick={handleOptions} type='button'>
+						<button value='6' onClick={handleOptions} type='button' className='unclicked-guest'>
 							6
 				</button>
 					</div>
@@ -78,37 +78,37 @@ export default function Guests(props: IGuestsProps) {
 				{showLastSix ? (
 					<div className="lastSix">
 						<div className="buttonContainer">
-							<button value='7' onClick={handleOptions} type='button'>
+							<button value='7' onClick={handleOptions} type='button' className='unclicked-guest'>
 								7
 				</button>
 						</div>
 
 						<div className="buttonContainer">
-							<button value='8' onClick={handleOptions} type='button'>
+							<button value='8' onClick={handleOptions} type='button' className='unclicked-guest'>
 								8
 				</button>
 						</div>
 
 						<div className="buttonContainer">
-							<button value='9' onClick={handleOptions} type='button'>
+							<button value='9' onClick={handleOptions} type='button' className='unclicked-guest'>
 								9
 				</button>
 						</div>
 
 						<div className="buttonContainer">
-							<button value='10' onClick={handleOptions} type='button'>
+							<button value='10' onClick={handleOptions} type='button' className='unclicked-guest'>
 								10
 				</button>
 						</div>
 
 						<div className="buttonContainer">
-							<button value='11' onClick={handleOptions} type='button'>
+							<button value='11' onClick={handleOptions} type='button' className='unclicked-guest'>
 								11
 				</button>
 						</div>
 
 						<div className="buttonContainer">
-							<button value='12' onClick={handleOptions} type='button'>
+							<button value='12' onClick={handleOptions} type='button' className='unclicked-guest'>
 								12
 				</button>
 						</div>
